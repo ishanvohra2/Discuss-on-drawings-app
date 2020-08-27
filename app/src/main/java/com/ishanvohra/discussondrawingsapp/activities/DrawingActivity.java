53629fb6a5e1878869496fc6efceeb81638f4737
ishanvohra2 <ishanvohra2@gmail.com>
2020-08-28 01:09:22 +0530

    Add New Marker Added

--- app/src/main/java/com/ishanvohra/discussondrawingsapp/activities/DrawingActivity.java
+++ app/src/main/java/com/ishanvohra/discussondrawingsapp/activities/DrawingActivity.java
@@ -1,5 +1,6 @@
 package com.ishanvohra.discussondrawingsapp.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -9,23 +10,42 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.ishanvohra.discussondrawingsapp.R;
+import com.ishanvohra.discussondrawingsapp.data.Marker;
+
+import java.io.ByteArrayOutputStream;
+
+import static java.security.AccessController.getContext;
 
 public class DrawingActivity extends AppCompatActivity {
 
     private FirebaseStorage storage = FirebaseStorage.getInstance();
+    private DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
 
     private ImageView imageView, markerImg;
+
+    private static final int PICK_IMAGE = 100;
+    private String drawingId;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -33,7 +53,7 @@ public class DrawingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawing);
 
-        String drawingId = getIntent().getStringExtra("drawingId");
+        drawingId = getIntent().getStringExtra("drawingId");
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -68,9 +88,88 @@ public class DrawingActivity extends AppCompatActivity {
     }
 
     private Dialog onCreateDialog(float x, float y){
-        Dialog dialog = new Dialog(this);
+        final Dialog dialog = new Dialog(this);
         dialog.setContentView(R.layout.add_marker_dialog_layout);
 
+        final EditText contentEt = dialog.findViewById(R.id.add_marker_dialog_content_et);
+        final ImageView imageView = dialog.findViewById(R.id.add_marker_dialog_iv);
+        Button addImgBtn = dialog.findViewById(R.id.add_marker_dialog_upload_img_btn);
+        Button cancelBtn = dialog.findViewById(R.id.add_marker_dialog_cancel_btn);
+        Button submitBtn = dialog.findViewById(R.id.add_marker_dialog_add_btn);
+
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        addImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGallery();
+            }
+        });
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Marker marker = new Marker();
+                marker.setMarkerId(databaseReference.push().getKey());
+                marker.setContentEt(contentEt.getText().toString());
+                marker.setDrawingId(drawingId);
+                marker.setType("Label");
+
+                if(imageView.getDrawable() != null){
+                    uploadImage(marker.getMarkerId());
+                }
+
+                databaseReference.child("markers").child(marker.getMarkerId()).setValue(marker).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(DrawingActivity.this, "Label added", Toast.LENGTH_SHORT).show();
+                            dialog.dismiss();
+                        }
+                    }
+                });
+            }
+        });
+
         return dialog;
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == PICK_IMAGE) {
+                imageView.setImageURI(data.getData());
+            }
+        }
+    }
+
+    public void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+
+
+    public String uploadImage(String markerId) {
+
+        if (imageView.getDrawable() != null) {
+            Bitmap bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+            byte[] bitmapdata = stream.toByteArray();
+
+            String path = "markers/" + markerId + "/image.jpeg";
+            StorageReference storageReference = storage.getReference(path);
+
+            UploadTask uploadTask = storageReference.putBytes(bitmapdata);
+
+            return path;
+        }
+
+        return "";
     }
 }
---
+++ app/src/main/java/com/ishanvohra/discussondrawingsapp/data/Marker.java
@@ -0,0 +1,37 @@
+package com.ishanvohra.discussondrawingsapp.data;
+
+public class Marker {
+    private String markerId, drawingId, type, contentEt;
+
+    public String getMarkerId() {
+        return markerId;
+    }
+
+    public void setMarkerId(String markerId) {
+        this.markerId = markerId;
+    }
+
+    public String getDrawingId() {
+        return drawingId;
+    }
+
+    public void setDrawingId(String drawingId) {
+        this.drawingId = drawingId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getContentEt() {
+        return contentEt;
+    }
+
+    public void setContentEt(String contentEt) {
+        this.contentEt = contentEt;
+    }
+}
